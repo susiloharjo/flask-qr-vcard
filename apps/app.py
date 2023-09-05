bb404dc157ed0dfe1ebfd35e5eb0adcf7201ae51
susiloharjo <susiloharjo@gmail.com>
2023-09-05 17:38:51 +0700

    coba

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,4 @@
-import qrcode
-import requests
-from flask import Flask, render_template, request
-import vcard
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -9,39 +6,32 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/barcode", methods=["POST"])
-def barcode():
-    global vcard
 
-    name = request.form.get("name")
-    company = request.form.get("company")
-    phone_number = request.form.get("phone_number")
+@app.route("/barcode", methods=["POST"])
+
+def generate_vcard():
+    # fn = "John Doe"
+    # org = "Company Name"
+    # tel = "+123456789"
+    # email = "john@example.com"
+    fn = request.form.get("name")
+    org = request.form.get("company")
+    tel = request.form.get("phone_number")
     email = request.form.get("email")
 
-    vcard = vcard.VCard()
-    vcard.add_name(name)
-    vcard.add_organization(company)
-    vcard.add_tel(phone_number)
-    vcard.add_email(email)
 
-    qrcode = qrcode.make(vcard.serialize(), encoding="utf-8", error_correction=qrcode.constants.ERROR_CORRECT_L)
 
-    filename = "barcode.png"
-    qrcode.save(filename)
+    vcard_data = f"""BEGIN:VCARD
+VERSION:3.0
+FN:{fn}
+ORG:{org}
+TEL:{tel}
+EMAIL:{email}
+END:VCARD
+    """
 
-    with open(filename, "rb") as f:
-        encoded_image = f.read()
-        base64_image = base64.b64encode(encoded_image)
+    return render_template('vcard_template.html', vcard_data=vcard_data)
 
-    os.remove(filename)
 
-    return {
-        "image": base64_image.decode("utf-8"),
-        "name": name,
-        "company": company,
-        "phone_number": phone_number,
-        "email": email,
-    }
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,req
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
